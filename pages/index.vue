ecfe3ae47c2573b739350ba807b3b3854a613cdc
spronghi <sim.colaci@gmail.com>
2020-09-17 10:26:47 +0200

    init nuxt with bulma

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container has-text-centered">
+    <h1 class="title">
+      Title
+    </h1>
+    <h2 class="subtitle">
+      Subtitle
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    script: [
+      {
+        src: 'https://kit.fontawesome.com/c1913ea72f.js'
+      }
+    ],
+  }
+}
+
+</script>
